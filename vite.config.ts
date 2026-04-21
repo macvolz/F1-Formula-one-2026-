2975cacb98137a335e23f1e8360c2a5051ffb49c
macvolz <macvolzgaming01@gmail.com>
2026-04-21 21:28:32 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  // This tells GitHub where to find your images and scripts
   base: './', 
   plugins: [react(), tailwindcss()],
   resolve: {
