efefdae09bcb14f9e7775d5f31ded678ad4c5c4f
macvolz <macvolzgaming01@gmail.com>
2026-04-21 21:33:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  // This is the most important line for GitHub Pages!
   base: './', 
   plugins: [react(), tailwindcss()],
   resolve: {
