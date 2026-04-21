51b0fe59716c21d8f93e1851d7e69916fcc1bbb0
macvolz <macvolzgaming01@gmail.com>
2026-04-21 21:48:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  // This must match your GitHub repository name exactly
+  // This must match your repo name exactly
   base: '/F1-Formula-one-2026-/', 
   plugins: [react(), tailwindcss()],
   resolve: {
